4db5fb330e237dec83868eab1d60225068c49210
Christoph Knabe <knabe@beuth-hochschule.de>
2020-03-24 15:56:20 +0100

    Success wrapping route POST /users/ with error reporting.

--- src/main/scala/com/zio/examples/http4s_doobie/http/Api.scala
+++ src/main/scala/com/zio/examples/http4s_doobie/http/Api.scala
@@ -3,7 +3,7 @@ package http
 
 import persistence._
 import io.circe.{Decoder, Encoder}
-import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes, Request}
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes, Request, Response}
 import org.http4s.dsl.Http4sDsl
 import zio._
 import org.http4s.circe._
@@ -16,37 +16,45 @@ final case class Api[R <: UserPersistence with Console](rootUri: String) {
   type UserTask[A] = RIO[R, A]
 
   implicit def circeJsonDecoder[A](implicit decoder: Decoder[A]): EntityDecoder[UserTask, A] = jsonOf[UserTask, A]
+
   implicit def circeJsonEncoder[A](implicit decoder: Encoder[A]): EntityEncoder[UserTask, A] = jsonEncoderOf[UserTask, A]
 
   val dsl: Http4sDsl[UserTask] = Http4sDsl[UserTask]
+
   import dsl._
 
   def route: HttpRoutes[UserTask] = {
 
     HttpRoutes.of[UserTask] {
-      case request @ GET -> Root / "1" =>
+      case request@GET -> Root / "1" =>
         val id = 1
-        /* This way the centralized reporting of failures works, but the execution trace shows only lines in
-        * service functions as `getUser` or `reporting`. It does not show the location in the PF branch. */
-        reporting(request){
+        domainReporting(request) {
           getUser(id)
         }
-      case request @ GET -> Root / IntVar(id) =>
-        val requestString = s"GET $rootUri/$id"
-        reporting(request){
-          getUser(id)
+      case request@GET -> Root / "2" =>
+        val id = 2
+        httpReporting(request) {
+          Ok(getUser(id))
         }
-      case request @ POST -> Root =>
+      /* This way the centralized reporting of failures works, but the execution trace shows only lines in
+      * service functions as `getUser` or `domainReporting`. It does not show the location in this branch. */
+      case request@GET -> Root / IntVar(id) => domainReporting(request) {
+        getUser(id)
+      }
+      case request@POST -> Root => httpReporting(request) {
         request.decode[User] { user =>
           Created(createUser(user))
         }
+      }
       case DELETE -> Root / IntVar(id) =>
         (getUser(id) *> deleteUser(id)).foldM(_ => NotFound(), Ok(_))
     }
   }
 
-  private def reporting(request: Request[UserTask])(logicEffect: => RIO[UserPersistence, User]) = {
-    logicEffect.foldCauseM(
+  /** Wraps the `domainEffect` into a failure reporting effect, which reports information about the request, and the ZIO execution trace
+   * about the Throwable by which the `domainEffect` failed. */
+  private def domainReporting(request: Request[UserTask])(domainEffect: RIO[UserPersistence, User]) = {
+    domainEffect.foldCauseM(
       failure = cause => {
         val method = request.method
         val uri = request.uri
@@ -60,14 +68,41 @@ final case class Api[R <: UserPersistence with Console](rootUri: String) {
     )
   }
 
+  /** Wraps the `httpEffect` into a failure reporting effect, which reports information about the request, and the ZIO execution trace
+   * about the Throwable by which the `httpEffect` failed. */
+  private def httpReporting0(request: Request[UserTask])(httpEffect: UserTask[Response[UserTask]]) = {
+    httpEffect.foldCauseM(
+      failure = cause => {
+        val method = request.method
+        val uri = request.uri
+        val messageHead = s"Request $method $uri failed with:\n"
+        putStrLn(failureTrace(messageHead, cause)) *>
+          InternalServerError(cause.failures.mkString(messageHead, "\n", ""))
+      },
+      success = result => RIO.succeed(result)
+    )
+  }
+
+  /** Wraps the `httpEffect` into a failure reporting effect, which reports information about the request, and the ZIO execution trace
+   * about the Throwable by which the `httpEffect` failed. */
+  private def httpReporting(request: Request[UserTask])(httpEffect: UserTask[Response[UserTask]]) = {
+    httpEffect.catchAllCause{cause =>
+      val method = request.method
+      val uri = request.uri
+      val messageHead = s"v3 Request $method $uri failed with:\n"
+      putStrLn(failureTrace(messageHead, cause)) *>
+        InternalServerError(cause.failures.mkString(messageHead, "\n", ""))
+    }
+  }
+
   /** Converts the given ZIO failure `cause` to a detailed diagnostic message containing the given `messageHead`,
    * the exceptions stack traces, and the execution traces and plans. */
   def failureTrace(messageHead: String, cause: Cause[Throwable]): String = {
     val lineIntro = "\n  at "
-    val failures = for{
+    val failures = for {
       failure <- cause.failures
       stackTrace = failure.getStackTrace.mkString(failure + lineIntro, lineIntro, "")
-      } yield stackTrace
+    } yield stackTrace
     val traces = cause.traces.map(_.prettyPrint).mkString("\n", "\n", "\n")
     failures.mkString(messageHead, "\n", "\n") + traces
   }
